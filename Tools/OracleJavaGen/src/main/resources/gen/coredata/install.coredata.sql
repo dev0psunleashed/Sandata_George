a93ba2e0552026f852c55322ee76f10f4e6a70a1
Pradeep Ganesh Raja <pradeep.ganesh@softcrylic.co.in>
2017-02-16 22:11:07 +0530

    checkin

--- Tools/OracleJavaGen/src/main/resources/gen/coredata/install.coredata.sql
+++ Tools/OracleJavaGen/src/main/resources/gen/coredata/install.coredata.sql
@@ -1,7 +1,2 @@
 @@"./global/global.sql";
-@@"./types/ADDR_PRIO_LKUP_T.sql";
-@@"./types/ADDR_TYP_LKUP_T.sql";
-@@"./types/ADMIN_STAFF_PT_T.sql";
-@@"./java/AdmPrd.java.sql";
-@@"./pkg/PKG_ADMIN.sql";
 EXIT;
